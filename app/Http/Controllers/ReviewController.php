3af56e4d91e855169eacf011e2667dd08573d191
Taidhghg <N00234049@iadt.ie>
2024-11-26 16:26:43 +0000

    class work

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -57,7 +57,10 @@ class ReviewController extends Controller
      */
     public function edit(Review $review)
     {
-        //
+        if(auth()->user()->id !== $review->user_id && auth()->user()->role !== 'admin'){
+            return redirect()->route('games.index')->with('error', 'Access denied.');
+        }
+        return view('reviews.edit', compact('review'));
     }
 
     /**
@@ -65,7 +68,11 @@ class ReviewController extends Controller
      */
     public function update(Request $request, Review $review)
     {
-        //
+        $review->update($request->only(['rating', 'comment']));
+
+        return redirect()->route('games.show', $review->game_id)
+        ->with('success', 'Review updated successfully.');
+
     }
 
     /**
